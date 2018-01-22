1d03038da6cbd44da35e50a982befd68399f3e36
Bastien <bastien.delbreil@lycee-bourdelle.fr>
2018-01-22 13:59:01 +0100

    [ADD] LE CODE

---
+++ ProgrammeConf/Conf sys/ConfSys/CSas.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "CSas.h"
+
+CSas::CSas(int nNbPrt)
+{
+}
+
+CSas::~CSas()
+{
+}
+
+int CSas::GetNbPrt() const { return m_nNbPrt; }
---
+++ ProgrammeConf/Conf sys/ConfSys/CSas.h
@@ -0,0 +1,11 @@
+class CSas
+{
+private:
+	int		m_nNbPrt;
+
+public:
+	CSas(int nNbPrt = 1);
+	~CSas();
+
+	int GetNbPrt() const;
+};
